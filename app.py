80caf55607908555a98bb74e982098e3197c9f35
Gaurav Dhangar <gauravkumar09470@gmail.com>
2026-01-22 21:28:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('CALCULATE YOUR BMI')
+h=1
+wt = st.number_input('Enter your weight in KGs:')
+h = st.number_input('Enter your height in CMs:')
+if h==0:
+    bmi = 0
+else:
+    bmi = wt/h**2
+st.success(f'Your BMI is {bmi} KG/cm^2')
